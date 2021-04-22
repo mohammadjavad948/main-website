33f6820363fa1568ce3919f67447a04f8fd7d67a
mohammadjavad <mjkmo123@gmail.com>
2021-04-22 12:36:37 +0430

    add text transition

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import TextTransition, {presets} from "react-text-transition";
+import {useEffect, useState} from "react";
 
 export default function Home() {
   return (
@@ -11,6 +13,7 @@ export default function Home() {
         <div className={styles.main}>
             <div>
                 <h1 className={styles.title}>mohammad javad</h1>
+                <Technology />
             </div>
             <Image
                 src={"/prof.png"}
@@ -22,4 +25,34 @@ export default function Home() {
         </div>
       </div>
   )
+}
+
+const TEXTS = [
+    "React",
+    "Angular",
+    "Laravel",
+    "Nodejs"
+];
+
+function Technology(){
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        const intervalId = setInterval(() =>
+                setIndex(index => index + 1),
+            1500 // every 3 seconds
+        );
+        return () => clearTimeout(intervalId);
+    }, []);
+
+    return (
+        <span>
+            <TextTransition
+                springConfig={presets.wobbly}
+                text={ TEXTS[index % TEXTS.length] }
+                inline
+            />
+            &nbsp; developer
+        </span>
+    )
 }
