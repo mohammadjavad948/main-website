0c11a17658ff021915ef45d74543baef44b54d43
mohammadjavad <mjkmo123@gmail.com>
2021-04-22 12:15:42 +0430

    add image

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -8,7 +9,12 @@ export default function Home() {
           <title>big javad</title>
         </Head>
         <div className={styles.main}>
-
+            <Image
+                src={"/prof.png"}
+                width={150}
+                height={150}
+                className={styles.image}
+            />
         </div>
       </div>
   )
