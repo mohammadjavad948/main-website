f37583169db45ac76a142a160eed64548f8042d2
mohammadjavad <mjkmo123@gmail.com>
2021-04-22 12:24:11 +0430

    add alt

--- pages/index.js
+++ pages/index.js
@@ -10,12 +10,13 @@ export default function Home() {
         </Head>
         <div className={styles.main}>
             <div>
-                <h1 className={styles.title}>'mohammad javad' also known as 'big javad'</h1>
+                <h1 className={styles.title}>mohammad javad</h1>
             </div>
             <Image
                 src={"/prof.png"}
                 width={150}
                 height={150}
+                alt={"profile image"}
                 className={styles.image}
             />
         </div>
